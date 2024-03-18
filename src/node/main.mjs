b88df79739319c1d498b2ae7663e03f8e9bcdebd
Simon Young <sjlyoung58@gmail.com>
2024-03-18 12:01:23 +0000

    added address for VPS deployed code

--- src/node/main.mjs
+++ src/node/main.mjs
@@ -1,12 +1,13 @@
 import zmq from 'zeromq'; // note needs npm install zeromq@6.0.0-beta.19 (or higher)
-const port = 5555;
+const port = 5551;
 
 var sock;
 
 async function run() {
   sock = new zmq.Subscriber;
 
-  sock.connect(`tcp://localhost:${port}`);
+//  sock.connect(`tcp://localhost:${port}`);
+  sock.connect(`tcp://srv477848.hstgr.cloud:${port}`);
   sock.subscribe('GalaxyTick');
   sock.subscribe('SystemTick');
   sock.subscribe('Heartbeat');
