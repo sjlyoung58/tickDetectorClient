389c75738b2c907129d7483c0050e58c66c29022
Simon Young <sjlyoung58@gmail.com>
2024-03-18 09:28:54 +0000

    initial commit

---
+++ src/node/main.mjs
@@ -0,0 +1,27 @@
+import zmq from 'zeromq'; // note needs npm install zeromq@6.0.0-beta.19 (or higher)
+const port = 5555;
+
+var sock;
+
+async function run() {
+  sock = new zmq.Subscriber;
+
+  sock.connect(`tcp://localhost:${port}`);
+  sock.subscribe('GalaxyTick');
+  sock.subscribe('SystemTick');
+  sock.subscribe('Heartbeat');
+  console.log(`Subscriber connected to port ${port}`);
+
+  for await (const [topic, msg] of sock) {
+    const sTopic = topic.toString();
+    const payload = msg.toString();
+    console.log(`${sTopic}: ${payload}`);
+  }
+}
+
+run();
+
+process.on('SIGINT', () => {
+  sock.close();
+  console.log("Subscriber shutting down...");
+});
